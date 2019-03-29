93d8798df5840bbda4325c5a137b6f20419c47fb
Christopher Baines <mail@cbaines.net>
2019-03-29 09:14:23 +0000

    More easily support stopping guix-data-service-process-jobs

--- guix-data-service/jobs.scm
+++ guix-data-service/jobs.scm
@@ -6,6 +6,8 @@
 (define (process-jobs conn)
   (while #t
     (match (process-next-load-new-guix-revision-job conn)
-      (#f (begin (simple-format #t "Waiting for new jobs...")
-                 (sleep 60)))
+      (#f (begin
+            (simple-format #t "Waiting for new jobs...")
+            (unless (eq? 0 (sleep 60))
+              (exit 0))))
       (_ #f))))
