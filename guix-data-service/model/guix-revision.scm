0c72ee747c253ea538032c6593683af6a54af5d3
Christopher Baines <mail@cbaines.net>
2019-04-08 21:03:59 +0100

    Add a function to get a count of Guix revisions

--- guix-data-service/model/guix-revision.scm
+++ guix-data-service/model/guix-revision.scm
@@ -1,10 +1,18 @@
 (define-module (guix-data-service model guix-revision)
+  #:use-module (srfi srfi-1)
   #:use-module (ice-9 match)
   #:use-module (squee)
-  #:export (most-recent-n-guix-revisions
+  #:export (count-guix-revisions
+            most-recent-n-guix-revisions
             commit->revision-id
             insert-guix-revision
             guix-revision-exists?))
+
+(define (count-guix-revisions conn)
+  (first
+   (exec-query
+    conn
+    "SELECT COUNT(*) FROM guix_revisions")))
 
 (define (most-recent-n-guix-revisions conn n)
   (exec-query conn "SELECT * FROM guix_revisions ORDER BY id DESC LIMIT 10"))
