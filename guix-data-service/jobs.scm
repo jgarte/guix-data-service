14cb5425d4d4eb87f636eb30e3bcae71414421f8
Christopher Baines <mail@cbaines.net>
2019-04-10 20:17:44 +0100

    Improve the job processing
    Speed up checking for new jobs, and remove debugging output.

--- guix-data-service/jobs.scm
+++ guix-data-service/jobs.scm
@@ -6,8 +6,6 @@
 (define (process-jobs conn)
   (while #t
     (match (process-next-load-new-guix-revision-job conn)
-      (#f (begin
-            (simple-format #t "Waiting for new jobs...")
-            (unless (eq? 0 (sleep 60))
-              (exit 0))))
-      (_ #f))))
+      (#f (unless (eq? 0 (sleep 5))
+            (exit 0)))
+      (_ (simple-format #t "\nFinished processing job\n\n")))))
